2a319b9ebbc3c3c86321733d6a967d777a6a279c
Yury Vdovin <savatop@yandex.ru>
2023-08-08 22:20:39 +0500

    add annotation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
 
     if char_class == 'warrior':
         return f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
@@ -12,7 +12,7 @@ def attack(char_name, char_class):
     return None
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
 
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
@@ -23,7 +23,7 @@ def defence(char_name, char_class):
     return None
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
 
     if char_class == 'warrior':
         return f'{char_name} применил специальное умение'
@@ -34,7 +34,7 @@ def special(char_name, char_class):
     return None
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -47,7 +47,7 @@ def start_training(char_name, char_class):
     print('defence — чтобы блокировать атаку противника')
     print('или special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -59,9 +59,9 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = ''
+    char_class: str = None
     while approve_choice != 'y':
         print('Введи за кого хочешь играть: ')
         char_class = input('Воитель — warrior, Маг — mage, Лекарь — healer: ')
